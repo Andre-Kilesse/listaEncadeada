25f3fadd939c85bec1cb4fe0dda4afa3a15c6725
Andre Kilesse <kilesseandre@gmail.com>
2025-03-22 01:06:10 -0300

    melhorias na reutilização do codigo e complexidade

--- script/una.js
+++ script/una.js
@@ -3,56 +3,74 @@ const Node = require('./node');
 class Una {
     constructor() {
         this.firstNode = null;
+        this.lastNode = null;
         this.totalElements = 0;
+    }
+
+    getPenultimateNode() {
+        if (this.totalElements < 2) {
+            return null;
+        } else {
+            let currentNode = this.firstNode;
+            while (currentNode.getNext() !== this.lastNode) {
+                currentNode = currentNode.getNext();
+            }
+            return currentNode; 
+        }
     }
 
     insertAtBeginning(value) {
         const newNode = new Node(value);
-        newNode.setNext(this.firstNode);
-        this.firstNode = newNode;
+        if(this.firstNode == null){
+            newNode.setNext(this.firstNode);
+            this.firstNode = newNode;
+            this.lastNode = newNode;
+        }else{
+            newNode.setNext(this.firstNode);
+            this.firstNode = newNode;
+        }
         this.totalElements++;
     }
 
     removeAtBeginning() {
-        if (this.totalElements === 0) {
+        if (this.totalElements == 0) {
             return null;
         }
         const aux = this.firstNode;
         this.firstNode = this.firstNode.getNext();
         this.totalElements--;
+        if(this.totalElements == 0){
+            this.lastNode = null;
+        }
         return aux;
     }
 
     insertAtEnding(value) {
         const newNode = new Node(value);
-
         if(this.firstNode == null){
             this.firstNode = newNode;
+            this.lastNode = newNode;
         } else {
-            let currentNode = this.firstNode;
-            while(currentNode.getNext() != null){
-                currentNode = currentNode.getNext();
-            }
-            currentNode.setNext(newNode);
+            this.lastNode.setNext(newNode);
+            this.lastNode = newNode;
         }
         this.totalElements++;
     }
 
     removeAtEnding() {
-        if (this.totalElements === 0) {
+        if (this.totalElements == 0) {
             return null;
-        } else if (this.firstNode.getNext() == null){
+        } else if (this.totalElements == 1){
             let aux = this.firstNode;
             this.firstNode = null;
+            this.lastNode = null    
             this.totalElements--;
             return aux;
         } else {
-            let currentNode = this.firstNode;
-            while(currentNode.getNext().getNext() != null){
-                currentNode = currentNode.getNext();
-            }
-            let aux = currentNode.getNext();
-            currentNode.setNext(null);
+            const penultimateNode = this.getPenultimateNode();
+            let aux = this.lastNode;
+            penultimateNode.setNext(null);
+            this.lastNode = penultimateNode;
             this.totalElements--;
             return aux;
         }
